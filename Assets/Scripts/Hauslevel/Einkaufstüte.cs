d9a00b330627d9109784685f7486c032123cd933
Lucamalka <lucamalka9@gmail.com>
2021-10-23 15:51:59 +0200

    Hi

--- Assets/Scripts/Hauslevel/Einkaufstüte.cs
+++ Assets/Scripts/Hauslevel/Einkaufstüte.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Einkaufstüte : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        public GameObject einkaufstuete;
     }
-
+    private void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+    }
     // Update is called once per frame
     void Update()
     {
